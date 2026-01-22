a18e8d7905614601b79d2d59656ae44e4e73e89c
Your Name <you@example.com>
2026-01-22 10:55:32 +0600

    feat: implement auth middleware for JWT verification and user retrieval

--- middlewares/authMiddleware.js
+++ middlewares/authMiddleware.js
@@ -1,2 +1,31 @@
 import jwt from jsonwebtoken;
-import {prisma} from 
+import {prisma} from "../database/prisma.js";
+
+export const authMiddleware = async (req, res, next) => {
+    const authHeader = req.headers.authorization;
+    if (!authHeader) {
+        return res.status(401).json({message: "Authorization header missing"});
+    }
+
+    const token = authHeader.split(" ")[1];
+    if (!token) {
+        return res.status(401).json({message: "Token missing"});
+    }
+    jwt.verify(token, process.env.JWT_SECRET, async (err, decoded) => {
+        if (err) {
+            return res.status(401).json({message: "Invalid token"});
+        }
+
+        const userId = decoded.userId;
+
+        const user = await prisma.users.findUnique({
+            where: {id: userId}
+        });
+        if (!user) {
+            return res.status(401).json({message: "User not found"});
+        }
+
+        req.user = user;
+        next();
+    });
+};
